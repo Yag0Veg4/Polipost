c914b2041fc129fd74b002e6b8295ecd80e860a3
DiegoBarajas <81001205+DiegoBarajas@users.noreply.github.com>
2022-11-08 19:08:17 -0600

    Quitar la kk

--- In-out/conexion.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $con=mysqli_connect('127.0.0.1','root','','polipost');
-    if(!$con){
-        echo 'no se conecto';
-    }else{
-        echo 'si se conecto';
-    }
-
-?>
--- app.js
+++
@@ -1,8 +0,0 @@
-function despliegue() {
-    var x = document.getElementById("links");
-    if(x.className === "navbar-colapsado") {
-        x.className += "responsive";
-    } else {
-        x.className = "navbar-colapsado";
-    }
-}
