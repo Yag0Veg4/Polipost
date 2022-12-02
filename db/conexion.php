3bd00ab82202a556f0d77f155983093d0ef457ea
SaulSerna <105526422+SaulSerna@users.noreply.github.com>
2022-12-02 16:53:36 -0600

    aaaaaaaa

--- db/conexion.php
+++ db/conexion.php
@@ -1,5 +1,5 @@
 <?php
-    $con = new mysqli("polipost.coe0irawvnp8.us-east-1.rds.amazonaws.com", "admin", "12345678", "polipost");
+    $con = new mysqli("polipost-02.coe0irawvnp8.us-east-1.rds.amazonaws.com", "admin", "12345678", "polipost");
     $con->set_charset("UTF8");
 
 ?>
